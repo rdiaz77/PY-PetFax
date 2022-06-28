a8cc2a2a9e39613622daa571007d4dd466f7be8a
RDM_ <rafadiazm77@gmail.com>
2022-06-27 21:29:16 -0500

    third commit

--- petfax/pet.py
+++ petfax/pet.py
@@ -5,6 +5,10 @@
 
 # imported render template in addition to Blueprint to render data into the index.html file
 from flask import (Blueprint, render_template)
+import json # to be able to open the pets.json file
+
+pets = json.load(open('pets.json')) # here we save the json package to a var called "pets" and used open() to open the file. This is a built-in function. And we used json.load() to decode JSON. This is why we had to import the json package. Notice that the json file is passes as str
+print(pets)
 
 # new instance of Blueprint
 bp = Blueprint ("pet", __name__, url_prefix="/pets" ) 
@@ -13,7 +17,10 @@ bp = Blueprint ("pet", __name__, url_prefix="/pets" )
 
 @bp.route('/')
 def index():
-    return "this is the pets index"
+    return render_template("index.html", pets = pets) # here I called the render_template() and passed the index.html file, so it renders what is in the index.html file. The additional variables, name and values, are passed w/o ""
+
+
+
 
 #now I need to register this route in the __init_py (factory) instance
 
