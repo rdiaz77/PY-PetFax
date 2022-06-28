b44b60048ef2a92142fe5478528e365b43de4fb3
RDM_ <rafadiazm77@gmail.com>
2022-06-27 20:45:42 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+# it is important to create a folder where to create the global application factory instance
+# here I bring the app to be used in this folder
+from petfax import create_app
+
+#create a new var with to execute the app factory function
+app = create_app()
+
+
+       
+
+
+
---
+++ petfax/__init__.py
@@ -0,0 +1,25 @@
+#environment configuration
+
+# python3 -m venv venv                
+# . venv/bin/activate   
+#then
+# pip install Flask                  
+
+# initial configuration to use Flask
+# index route
+# As we just learned, to create a route we need to call .route() on the app instance.
+# The method expects at least one argument specifying what endpoint to use for the route. Let's just have it go to '/'
+# To tell our route what to do when that endpoint is used, we need to define a method directly underneath it. Let's name our method index
+
+# create global application factory instance
+# the folder containing the app factory has to be created at the same level of the file that will be using the instance
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    @app.route('/')
+    def hello():
+        return "hello, PetFax"
+    
+    return app
+    
